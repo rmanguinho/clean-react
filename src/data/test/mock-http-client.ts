95c6283f60c7eba54cf16c0ff836ea6e7e65ede8
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-06 18:19:37 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,12 +1,16 @@
 import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
+import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: object
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent
+  }
 
-  async post (params: HttpPostParams): Promise<void> {
+  async post (params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url
     this.body = params.body
-    return Promise.resolve()
+    return Promise.resolve(this.response)
   }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,8 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import faker from 'faker'
 
 type SutTypes = {
@@ -30,5 +32,14 @@ describe('RemoteAuthentication', () => {
     const authenticationParams = mockAuthentication()
     await sut.auth(authenticationParams)
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unathorized
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })
