afd70516358ee2710d967cc2e058c9412d696d7a
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-15 12:21:47 -0300

    test: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -0,0 +1,9 @@
+import { RequiredFieldValidation } from '@/validation/validators'
+import { ValidationBuilder as sut } from './validation-builder'
+
+describe('ValidationBuilder', () => {
+  test('Should return RequiredFieldValidation', () => {
+    const validations = sut.field('any_field').required().build()
+    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+})
