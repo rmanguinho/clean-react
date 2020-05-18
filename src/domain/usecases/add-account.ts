4b910b5369fd5365cb84ae69163644aed81f3ee9
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-18 16:28:43 -0300

    feat: add AddAccount usecase

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,12 @@
+import { AccountModel } from '@/domain/models/account-model'
+
+export type AddAccountParams = {
+  name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+export interface AddAccount {
+  add: (params: AddAccountParams) => Promise<AccountModel>
+}
