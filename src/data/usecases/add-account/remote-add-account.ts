e03f40f277c4f414d63c0773ef8f66a75d042cab
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-18 16:59:06 -0300

    feat: ensure RemoteAddAccount return an AccountModel on success

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -15,7 +15,7 @@ export class RemoteAddAccount implements AddAccount {
       body: params
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return null
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.forbidden: throw new EmailInUseError()
       default: throw new UnexpectedError()
     }
