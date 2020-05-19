567cb6cca35c386713fafd260ef97338d09424df
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-19 16:49:09 -0300

    test: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -133,4 +133,11 @@ describe('SignUp Component', () => {
       passwordConfirmation: password
     })
   })
+
+  test('Should call AddAccount only once', async () => {
+    const { sut, addAccountSpy } = makeSut()
+    await simulateValidSubmit(sut)
+    await simulateValidSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(1)
+  })
 })
--- src/presentation/test/mock-add-account.ts
+++ src/presentation/test/mock-add-account.ts
@@ -5,9 +5,11 @@ import { mockAccountModel } from '@/domain/test'
 export class AddAccountSpy implements AddAccount {
   account = mockAccountModel()
   params: AddAccountParams
+  callsCount = 0
 
   async add (params: AddAccountParams): Promise<AccountModel> {
     this.params = params
+    this.callsCount++
     return this.account
   }
 }
