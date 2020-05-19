9b09b9b7a4d0a2b0173f1e01de9c13fec8b912ee
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-19 17:55:01 -0300

    test: ensure Login link goes to the right place

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -183,4 +183,12 @@ describe('SignUp Component', () => {
     Helper.testElementText(sut, 'main-error', error.message)
     Helper.testChildCount(sut, 'error-wrap', 1)
   })
+
+  test('Should go to login page', () => {
+    const { sut } = makeSut()
+    const loginLink = sut.getByTestId('login-link')
+    fireEvent.click(loginLink)
+    expect(history.length).toBe(1)
+    expect(history.location.pathname).toBe('/login')
+  })
 })
