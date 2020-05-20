394e2a8d9411a6958d3b560f669bcdc91cc9aab9
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-20 10:56:06 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,11 @@
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  return ValidationComposite.build([
+    ...Builder.field('name').required().min(5).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().min(5).build(),
+    ...Builder.field('passwordConfirmation').required().min(5).build()
+  ])
+}
