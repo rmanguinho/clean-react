b0f1f748476ecce89c12cb4bd8e831e9bf49554c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-22 11:21:06 -0300

    refactor: improve input layout

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -9,26 +9,28 @@ const Input: React.FC<Props> = (props: Props) => {
   const inputRef = useRef<HTMLInputElement>()
   const error = state[`${props.name}Error`]
   return (
-    <div className={Styles.inputWrap}>
+    <div
+      data-testid={`${props.name}-wrap`}
+      className={Styles.inputWrap}
+      data-status={error ? 'invalid' : 'valid'}
+    >
       <input
         {...props}
         ref={inputRef}
+        title={error}
         placeholder=" "
         data-testid={props.name}
         readOnly
         onFocus={e => { e.target.readOnly = false }}
         onChange={e => { setState({ ...state, [e.target.name]: e.target.value }) }}
       />
-      <label onClick={() => { inputRef.current.focus() }}>
+      <label
+        data-testid={`${props.name}-label`}
+        onClick={() => { inputRef.current.focus() }}
+        title={error}
+      >
         {props.placeholder}
       </label>
-      <span
-        data-testid={`${props.name}-status`}
-        title={error || 'Tudo certo!'}
-        className={Styles.status}
-      >
-        {error ? '🔴' : '🟢'}
-      </span>
     </div>
   )
 }
--- src/presentation/test/form-helper.tsx
+++ src/presentation/test/form-helper.tsx
@@ -11,10 +11,13 @@ export const testButtonIsDisabled = (sut: RenderResult, fieldName: string, isDis
   expect(button.disabled).toBe(isDisabled)
 }
 
-export const testStatusForField = (sut: RenderResult, fieldName: string, validationError?: string): void => {
-  const fieldStatus = sut.getByTestId(`${fieldName}-status`)
-  expect(fieldStatus.title).toBe(validationError || 'Tudo certo!')
-  expect(fieldStatus.textContent).toBe(validationError ? '🔴' : '🟢')
+export const testStatusForField = (sut: RenderResult, fieldName: string, validationError: string = ''): void => {
+  const wrap = sut.getByTestId(`${fieldName}-wrap`)
+  const field = sut.getByTestId(fieldName)
+  const label = sut.getByTestId(`${fieldName}-label`)
+  expect(wrap.getAttribute('data-status')).toBe(validationError ? 'invalid' : 'valid')
+  expect(field.title).toBe(validationError)
+  expect(label.title).toBe(validationError)
 }
 
 export const populateField = (sut: RenderResult, fieldName: string, value = faker.random.word()): void => {
