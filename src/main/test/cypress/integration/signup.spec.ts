43619b270f5fd6819ba2d370dddd260e3de4d045
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-22 14:57:08 -0300

    test: ensure SignUp save accessToken on localstorage on success

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -76,4 +76,12 @@ describe('SignUp', () => {
     FormHelper.testMainError('Algo de errado aconteceu. Tente novamente em breve.')
     FormHelper.testUrl('/signup')
   })
+
+  it('Should present save accessToken if valid credentials are provided', () => {
+    Http.mockOk()
+    simulateValidSubmit()
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+    FormHelper.testUrl('/')
+    FormHelper.testLocalStorageItem('accessToken')
+  })
 })
--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -4,3 +4,4 @@ import faker from 'faker'
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError(/signup/)
 export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/signup/, 'POST')
 export const mockInvalidData = (): void => Helper.mockOk(/signup/, 'POST', { invalid: faker.random.uuid() })
+export const mockOk = (): void => Helper.mockOk(/signup/, 'POST', { accessToken: faker.random.uuid() })
