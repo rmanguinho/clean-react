74a80133da61e6e0c4c85a201315c9242014509d
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-26 15:32:48 -0300

    feat: ensure RemoteLoadSurveyList returns a list on 200

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,16 +1,18 @@
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
 import { UnexpectedError } from '@/domain/errors'
+import { SurveyModel } from '@/domain/models'
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
 
-export class RemoteLoadSurveyList {
+export class RemoteLoadSurveyList implements LoadSurveyList {
   constructor (
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<SurveyModel[]>
   ) {}
 
-  async loadAll (): Promise<void> {
+  async loadAll (): Promise<SurveyModel[]> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       default: throw new UnexpectedError()
     }
   }
--- src/domain/models/survey-model.ts
+++ src/domain/models/survey-model.ts
@@ -1,10 +1,12 @@
 export type SurveyModel = {
   id: string
   question: string
-  answers: [{
-    image?: string
-    answer: string
-  }]
+  answers: SurveyAnswerModel[]
   date: Date
   didAnswer: boolean
+}
+
+export type SurveyAnswerModel = {
+  image?: string
+  answer: string
 }
