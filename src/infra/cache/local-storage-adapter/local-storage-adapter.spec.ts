ee57a6b54f8160ec064ad7c129375ea74325c837
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-02 15:34:18 -0300

    test: ensure LocalStorageAdapter calls remoteItem if value is null

--- src/infra/cache/local-storage-adapter/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter/local-storage-adapter.spec.ts
@@ -17,6 +17,13 @@ describe('LocalStorageAdapter', () => {
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
   })
 
+  test('Should call localStorage.removeItem if value is null', async () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    sut.set(key, undefined)
+    expect(localStorage.removeItem).toHaveBeenCalledWith(key)
+  })
+
   test('Should call localStorage.getItem with correct value', async () => {
     const sut = makeSut()
     const key = faker.database.column()
