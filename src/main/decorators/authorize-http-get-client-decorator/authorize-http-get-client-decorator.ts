e702c35ad2424cca0ea2060ca110af5289df5109
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-02 08:36:03 -0300

    feat: ensure AuthorizeHttpGetClientDecorator add token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -8,7 +8,14 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   ) {}
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
-    this.getStorage.get('account')
+    const account = this.getStorage.get('account')
+    if (account?.accessToken) {
+      Object.assign(params, {
+        headers: {
+          'x-access-token': account.accessToken
+        }
+      })
+    }
     await this.httpGetClient.get(params)
     return null
   }
