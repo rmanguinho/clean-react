960b4a6274faebafd3e7c796ac4442368579d17f
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-08 18:31:09 -0300

    feat: ensure RemoteLoadSurveyResult throws UnexpectedError on 404

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,5 +1,5 @@
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 
 export class RemoteLoadSurveyResult {
   constructor (
@@ -11,7 +11,8 @@ export class RemoteLoadSurveyResult {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: break
-      default: throw new AccessDeniedError()
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      default: throw new UnexpectedError()
     }
   }
 }
