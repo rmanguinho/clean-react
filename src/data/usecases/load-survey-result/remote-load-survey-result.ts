12aa85243ece48c8d05b3da0acb583183e1c14ae
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-08 18:29:00 -0300

    feat: ensure RemoteLoadSurveyResult throws AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,4 +1,5 @@
-import { HttpGetClient } from '@/data/protocols/http'
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError } from '@/domain/errors'
 
 export class RemoteLoadSurveyResult {
   constructor (
@@ -7,6 +8,10 @@ export class RemoteLoadSurveyResult {
   ) {}
 
   async load (): Promise<void> {
-    await this.httpGetClient.get({ url: this.url })
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      default: throw new AccessDeniedError()
+    }
   }
 }
