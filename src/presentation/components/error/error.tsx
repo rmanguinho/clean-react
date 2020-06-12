ed2da53061a0ca14c7f5ff155481b682a27136a7
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-12 10:28:54 -0300

    refactor: move Error to its own Component

---
+++ src/presentation/components/error/error.tsx
@@ -0,0 +1,18 @@
+import Styles from './error-styles.scss'
+import React from 'react'
+
+type Props = {
+  error: string
+  reload: () => void
+}
+
+const Error: React.FC<Props> = ({ error, reload }: Props) => {
+  return (
+    <div className={Styles.errorWrap}>
+      <span data-testid="error">{error}</span>
+      <button data-testid="reload" onClick={reload}>Tentar novamente</button>
+    </div>
+  )
+}
+
+export default Error
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,6 @@
 import Styles from './survey-list-styles.scss'
-import { Header, Footer } from '@/presentation/components'
-import { SurveyContext, SurveyListItem, Error } from '@/presentation/pages/survey-list/components'
+import { Header, Footer, Error } from '@/presentation/components'
+import { SurveyContext, SurveyListItem } from '@/presentation/pages/survey-list/components'
 import { useErrorHandler } from '@/presentation/hooks'
 import { LoadSurveyList } from '@/domain/usecases'
 import React, { useEffect, useState } from 'react'
@@ -18,6 +18,7 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
     error: '',
     reload: false
   })
+  const reload = (): void => setState(old => ({ surveys: [], error: '', reload: !old.reload }))
 
   useEffect(() => {
     loadSurveyList.loadAll()
@@ -31,7 +32,10 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <SurveyContext.Provider value={{ state, setState }}>
-          {state.error ? <Error /> : <SurveyListItem />}
+          {state.error
+            ? <Error error={state.error} reload={reload} />
+            : <SurveyListItem />
+          }
         </SurveyContext.Provider>
       </div>
       <Footer />
