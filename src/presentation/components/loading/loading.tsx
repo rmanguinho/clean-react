fee0225c220c6a01bc0ef8f30391673a07f36959
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-06-15 15:40:42 -0300

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/components/loading/loading.tsx
+++ src/presentation/components/loading/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Loading: React.FC = () => {
   return (
-    <div className={Styles.loadingWrap}>
+    <div data-testid="loading" className={Styles.loadingWrap}>
       <div className={Styles.loading}>
         <span>Aguarde...</span>
         <Spinner isNegative />
--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,14 +1,21 @@
 import Styles from './survey-result-styles.scss'
-import { Header, Footer, Loading, Calendar } from '@/presentation/components'
+import { Header, Footer, Loading, Calendar, Error } from '@/presentation/components'
+import { LoadSurveyResult } from '@/domain/usecases'
 import FlipMove from 'react-flip-move'
-import React from 'react'
+import React, { useState } from 'react'
 
 const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as LoadSurveyResult.Model
+  })
+
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
-      <div className={Styles.contentWrap}>
-        {true &&
+      <div data-testid="survey-result" className={Styles.contentWrap}>
+        {state.surveyResult &&
           <>
             <hgroup>
               <Calendar date={new Date()} className={Styles.calendarWrap} />
@@ -34,7 +41,8 @@ const SurveyResult: React.FC = () => {
             <button>Voltar</button>
           </>
         }
-        { false && <Loading /> }
+        { state.isLoading && <Loading /> }
+        { state.error && <Error error={state.error} reload={() => {}} /> }
       </div>
       <Footer />
     </div>
